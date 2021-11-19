b55dae327fc22c721902f359caf60cd5850da45f
Tetsuro-11 <n.tetsurou11@gmail.com>
2021-11-19 15:23:27 +0000

    add edit feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -17,6 +18,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   private
